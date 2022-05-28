612ec294247b545dbd9e66aeaad26199cd555827
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-05-28 10:58:56 +0300

    Update question12.sql

--- question12.sql
+++ question12.sql
@@ -14,6 +14,11 @@ WHERE rental_rate = (SELECT MIN (rental_rate) FROM film)
 AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
 
 -- Question 4
-SELECT customer_id, SUM(amount) AS Toplam Alisveris FROM payment 
-GROUP BY customer_id 
-ORDER BY SUM(amount) DESC;
+SELECT COUNT(payment_id) AS En_fazla_alim,
+(
+SELECT first_name FROM customer 
+WHERE payment.customer_id = customer.customer_id
+) 
+FROM payment 
+GROUP BY customer_id
+ORDER BY COUNT(payment_id) DESC;
