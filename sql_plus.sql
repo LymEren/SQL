091c567da38b53780c56c3b931abecdfa8c82af2
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-09-27 11:24:16 +0300

    Update sql_plus.sql

--- sql_plus.sql
+++ sql_plus.sql
@@ -68,3 +68,59 @@ WHERE adi = 'MEHMET'
 
 
 
+------------
+
+
+UPDATE employees
+   SET job_id =
+       (SELECT job_id FROM employees WHERE employee_ıd = 101),
+       salary =
+       (SELECT salary FROM employees WHERE employee_ıd = 101)
+ WHERE employee_id = 103;
+
+DELETE FROM countrıes
+WHERE country_name = 'Türkiye';
+
+MERGE INTO departments U1
+USING (Select * from departments_cp) U2
+ON (U1.department_id = U2.department_id)
+WHEN MATCHED THEN 
+  UPDATE SET U1.department_name = u2.department_name
+  WHEN NOT MATCHED THEN
+    INSERT                        
+    VALUES(U2.department_id, U2.department_name, U2.manager_id, U2.location_id)
+
+-------------------
+
+INSERT INTO countrıes (country_ıd, country_name, regıon_ıd)
+VALUES ('TR', 'Türkiye', 1);
+1 rows inserted
+
+INSERT INTO countrıes (country_ıd, country_name, regıon_ıd)
+VALUES ('GR', 'Almanya', 1);
+1 rows inserted
+
+SAVEPOINT ulkeler_eklendi
+
+INSERT INTO managers(managerID, managerName, salary, commission_pct)
+  SELECT employee_id, last_name, salary + 400, commission_pct * 0.2
+  FROM   employees
+  WHERE  job_id LIKE '%MAN%';
+11 rows inserted
+
+SAVEPOINT yoneticiler_eklendi
+
+UPDATE   employees
+SET      job_id  = (SELECT  job_id 
+                    FROM    employees 
+                    WHERE   employee_ıd = 101), 
+         salary  = (SELECT  salary 
+                    FROM    employees 
+                    WHERE   employee_ıd = 101) 
+WHERE    employee_id    =  103;
+1 rows updated
+
+SAVEPOINT maaslar_guncellendi
+
+
+ROLLBACK TO yoneticiler_eklendi;
